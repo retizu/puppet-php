dd3866c15fa2b3650013d9f93861e4f93ff1ca20
Catalin Panaite <catalin.gf@gmail.com>
2015-02-28 01:37:16 +0000

    fix augeas creating config before php package install

--- manifests/augeas.pp
+++ manifests/augeas.pp
@@ -65,12 +65,14 @@ define php::augeas (
   $changes = $ensure ? {
     present => [ "set '${entry}' '${value}'" ],
     absent  => [ "rm '${entry}'" ],
+    require => Package['php'],
   }
 
   augeas { "php_ini-${name}":
     incl    => $target,
     lens    => 'Php.lns',
     changes => $changes,
+    require => Package['php'],
   }
 
 }
